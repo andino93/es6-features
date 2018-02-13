bdf76ad856c71ddb69cf185e8cccc1f7ac6fbba6
andino <andy.andino93@gmail.com>
2018-02-13 11:04:38 -0800

    add block scope examples for variable declarations

---
+++ es6/variableDec.js
@@ -0,0 +1,21 @@
+/* eslint no-var: off, prefer-const: off, block-scoped-var: off, vars-on-top: off,
+no-redeclare: off, no-shadow: off, no-console: off */
+
+const varScopes = () => {
+  var x = 1;
+  let y = 2;
+  const z = 3;
+  {
+    var x = 100;
+    let y = 200;
+    const z = 300;
+    console.log('x in block scope is', x);
+    console.log('y in block scope is', y);
+    console.log('z in block scope is', z);
+  }
+  console.log('x outside of block scope is', x);
+  console.log('y outside of block scope is', y);
+  console.log('z outside of block scope is', z);
+};
+
+varScopes();
