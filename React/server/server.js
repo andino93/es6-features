93f55a37d6f89c682e9987d03ded0647713cb0a7
andino <andy.andino93@gmail.com>
2018-02-12 16:41:05 -0800

    change server listen port to 3000

--- React/server/server.js
+++ React/server/server.js
@@ -1,21 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import webpack from 'webpack';
+import devMiddleware from 'webpack-dev-middleware';
+import hotMiddleware from 'webpack-hot-middleware';
+import config from '../webpack.dev.config';
 
 const server = express();
 const assets = path.join(__dirname, '../client/');
-const port = 9800;
+const port = 3000;
 
 server.listen(port, () => console.log(`server is listening on ${port}`));
 
 /* eslint-disable */
-const config = require('../webpack.dev.config.js');
-const compiler = require('webpack')(config);
-server.use(require('webpack-dev-middleware')(compiler, {
+const compiler = webpack(config);
+server.use(devMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
 }));
-server.use(require('webpack-hot-middleware')(compiler));
+server.use(hotMiddleware(compiler));
 /* eslint-enable */
 
 server.use(express.static(assets));
