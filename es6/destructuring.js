cc25d30ff5d55543c0e3ffed328ea3d951ecdcaf
andino <andy.andino93@gmail.com>
2018-02-12 23:38:31 -0800

    add exmaple destructering

---
+++ es6/destructuring.js
@@ -0,0 +1,41 @@
+const printFirstAndSecondElement = ([first, second]) => {
+  console.log(`First element is ${first}, second is ${second}`);
+};
+
+const printSecondAndFourthElement = ([, second, , fourth]) => {
+  console.log(`Second element is ${second}, fourth is ${fourth}`);
+};
+
+const array = [1, 2, 3, 4, 5];
+
+printFirstAndSecondElement(array);
+printSecondAndFourthElement(array);
+
+const printBasicInfo = ({ firstName, lastName, profession }) => {
+  console.log(`${firstName} ${lastName} - ${profession}`);
+};
+
+const person = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 33,
+  children: 3,
+  profession: 'teacher',
+};
+
+printBasicInfo(person);
+
+const printAgeAndProfession = (adult) => {
+  const { age, profession } = adult;
+  console.log(`This person is ${age} years old and in this ${profession}`);
+};
+
+printAgeAndProfession(person);
+
+const printFirstAndLastElement = (arr) => {
+  const [first, , , , last] = arr;
+
+  console.log(`First element is ${first} and the last element is ${last}`);
+};
+
+printFirstAndLastElement(array);
