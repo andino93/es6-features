e46f5375bc4a1a5103d4f8cb5a14bdac19a23810
andino <andy.andino93@gmail.com>
2018-02-14 18:25:31 -0800

    add get endpoint and all endpoints functions

--- es6/async.js
+++ es6/async.js
@@ -4,18 +4,37 @@ const baseUrl = 'https://jsonplaceholder.typicode.com';
 const endpoints = {
   posts: '/posts',
   comments: '/comments',
-  albumns: '/albumns',
+  albumns: '/albums',
   photos: '/photos',
   users: '/users',
 };
 
-const getComments = async () => {
+// get users from by promise
+const getEndpoint = endpoint => (
+  axios.get(`${baseUrl}${endpoint}`)
+    .then(users => console.log(users.data))
+    .catch(error => console.error('error in user get: ', error))
+);
+// getEndpoint(endpoints.users);
+
+const hitAllEndpoints = () => {
+  const promises = Object.values(endpoints).map(endpoint => axios.get(`${baseUrl}${endpoint}`));
+  return Promise.all(promises)
+    .then(success => success.map(response => response.data)) // format data from response
+    .then(data => console.log(data))
+    .catch(error => console.error('error in hitAllEndpoints', error));
+};
+// hitAllEndpoints();
+
+const getUsers = async () => {
   try {
     const response = await axios.get(`${baseUrl}${endpoints.users}`);
-    console.log(response)
+    console.log(response.data);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
 
-getComments()
+// getUsers()
+
+export { getEndpoint, getUsers, hitAllEndpoints };
